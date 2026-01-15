0556e47d35b03ad5c8f516ec1df2a352427b62ff
4-EBULL <4-EBULL@BH-SFSC-02.local.gfmat.org>
2026-01-15 10:06:22 +0000

    ...

--- src/ViewAssignmentsScreen/GettingAssignmentsForTheTable.java
+++ src/ViewAssignmentsScreen/GettingAssignmentsForTheTable.java
@@ -4,6 +4,12 @@
  */
 package ViewAssignmentsScreen;
 
+import static ConnectTheDatabase.ConnectTheDatabase.TheConnectionToDatabase;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author 4-EBULL
@@ -11,5 +17,34 @@ package ViewAssignmentsScreen;
 public class GettingAssignmentsForTheTable {
     // to set column names
     // get topics. 
-    //jTableName.addItem
+    //jTableName.addItem 
+    // wjhat i want in the table = id, done, resource or quiz, num of questoins, percent done, date, topic.
+    
+    
+    
+    ///////////// THIS IS UNFINISHED I DIDNT HAVE ACCESS TO MY SQL SO FIX IT !!
+    ///
+    ///
+    ///
+    ///
+    ///
+    ///
+    ///
+    // so im using this for the colum names
+    public static ArrayList<String> ColumsInTable(int StudentID){
+        ArrayList<String> ListOfColumnNames = new ArrayList<>();
+        try (Connection connection = TheConnectionToDatabase()){
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT AssignmentInfoId,Resource, NumOfQuizQuestions, quiz_percentage, DueDate, Done FROM AssignmentInfo WHERE Assigned_id = (SELECT AssignedId FROM Assigned WHERE StudentId = " + StudentID + ")");
+            while (results.next()){
+                String TopicForList = results.getString("Topic");
+                ListOfColumnNames.add(TopicForList);
+            }
+            return ListOfColumnNames;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
